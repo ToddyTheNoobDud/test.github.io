b717c768eee765cc27bee63a1d214d303055a533
toddynnn <86982643+ToddyTheNoobDud@users.noreply.github.com>
2025-07-31 18:35:35 -0300

    fuck you

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,8 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/', // Change this from '/test.github.io/' to '/'
+  base: '/test.github.io/',
   server: {
     host: "::",
     port: 8080,
